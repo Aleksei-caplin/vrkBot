b2fd151af8575bbbbd5035148d9e66e590d65f1a
Alex <temp125@br.ru>
2020-07-13 16:39:49 +0700

    Conversation Bot

--- bot.py
+++ bot.py
@@ -1,56 +1,137 @@
 from pprint import pprint
-from telegram import Update, ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
-from telegram.ext import Updater, CallbackContext, Filters, MessageHandler
+from urllib import parse
+from urllib.request import Request, urlopen
+
+
+import requests
+from requests import HTTPError
+from telegram import Bot, Update, ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove, InlineKeyboardMarkup, InlineKeyboardButton
+from telegram.ext import Updater, CallbackContext, Filters, MessageHandler, CommandHandler, ConversationHandler
 
 import bot_config
 
-button_rest = 'Проверить номер телефона'
+button_subscription = "Активировать подписку"
+button_cancel = "Выйти из диалога"
+button_ok = "Ok"
+
+PHONE, FINISH = range(2)
 
 
-def button_rest_handler(update: Update, context: CallbackContext):
-    text = update.message.text
-    update.message.reply_text(
-        text="Ответ",
-        reply_markup=ReplyKeyboardRemove()
+def default_handler(update: Update, context: CallbackContext):
+    """ Встречаем пользователя и предлогаем начать диалог командой /start """
+    pprint("default")
+    context.bot.send_message(
+        chat_id=update.effective_message.chat_id,
+        text='Нажмите /start для заполнения анкеты!'
+        #reply_markup=ReplyKeyboardRemove()
     )
-    pprint(text)
+
+    return PHONE
 
 
-def message_handler(update: Update, context: CallbackContext):
-    """
-        обработка текстовых сообщений
-    update - событие которое пришло из API телеграмма
-    context - пользовательский контекст
-    """
-    # определяем что нажата кнопка
-    text = update.message.text
-    if text == button_rest:
-        return button_rest_handler(update=update, context=context)
+def start_handler(update: Update, context: CallbackContext):
+    """ Активация бота при вводе команды /start """
+    pprint("start")
 
-    # добавляем клавиатуру
+    # добавляем клавиатуру под поле ввода
     reply_markup = ReplyKeyboardMarkup(
         keyboard=[
             [
-                KeyboardButton(text=button_rest)
+                KeyboardButton(text="Отправить номер телефона", request_contact=True),
+                KeyboardButton(text=button_cancel)
             ],
         ],
         resize_keyboard=True
     )
 
-    # ответ бота
-    update.message.reply_text(
-        text=f"Здравствуйте {update.message.from_user.first_name} для подтверения подписки укажите контактный телефон",
-        reply_markup=reply_markup,
+    # добавляем клавиатуру под сообщение
+    inline_buttons = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [
+                InlineKeyboardButton(text=button_subscription, callback_data="subscribe", request_contact=True),
+            ],
+            [
+                InlineKeyboardButton(text=button_cancel, callback_data="cancel"),
+            ]
+        ],
     )
 
+    context.bot.send_message(
+        chat_id=update.effective_message.chat_id,
+        text=f"Здравствуйте {update.message.from_user.first_name} для подтверения подписки подтвердите контактный телефон",
+        reply_markup=reply_markup
+    )
+
+    return PHONE
+
+
+def phone_handler(update: Update, context: CallbackContext):
+    """ Отправка телефона пользователем """
+    pprint("phone")
+    # получаем ответ пользователя
+    user_phone = update.message.contact.phone_number
+    user_chat_id = update.message.contact.user_id
+
+    # отправляем запрос на сервер
+    try:
+        response = requests.post(bot_config.URL, data={'phone': user_phone, 'chat_id': user_chat_id}).json()
+        if response["status"] == 200:
+            text_to_user = "Запрос на подписку прошел успешно"
+        else:
+            text_to_user = "Пользователя с таким телефоном не найдено. За дополнительной информацией, обратитесь к админу"
+        pprint(response)
+    except requests.exceptions.HTTPError as e:
+        text_to_user = f"Запрос на подписку прошел не успешно. Возможные ошибки {e.strerror}"
+
+    context.bot.send_message(
+        chat_id=update.effective_message.chat_id,
+        text=text_to_user,
+        reply_markup=ReplyKeyboardRemove()
+    )
+
+    return ConversationHandler.END
+
+
+def finish_handler(update: Update, context: CallbackContext):
+    """ сообытие после отправки контакта """
+    context.user_data[PHONE] = update.message.text
+    pprint("finish")
+    pprint(context.user_data[PHONE])
+
+    context.bot.send_message(
+        chat_id=update.effective_message.chat_id,
+        text="Ваш контакт отправлен в базу и подписка активирована",
+    )
+
+    return ConversationHandler.END
+
+
+def cancel_handler(update: Update, context: CallbackContext):
+    """ Отменить весь процесс диалога. Данные будут утеряны """
+    pprint("cancel")
 
 
 def main():
     """ Основная функция (точкка входа) """
     updater = Updater(token=bot_config.TOKEN, use_context=True)
 
-    # подключаем слушатель события отправки сообщения
-    updater.dispatcher.add_handler(MessageHandler(filters=Filters.all, callback=message_handler))
+    # подключаем метод для диалогов и обработки команд
+    conversation_handler = ConversationHandler(
+        entry_points=[
+            CommandHandler('start', start_handler)
+        ],
+        states={
+            PHONE: [
+                MessageHandler(Filters.all, phone_handler, pass_user_data=True),
+            ],
+        },
+        fallbacks=[
+            CommandHandler('cancel', cancel_handler),
+        ]
+    )
+
+    updater.dispatcher.add_handler(conversation_handler)
+    updater.dispatcher.add_handler(MessageHandler(filters=Filters.all, callback=default_handler))
 
     updater.start_polling()
     updater.idle()
